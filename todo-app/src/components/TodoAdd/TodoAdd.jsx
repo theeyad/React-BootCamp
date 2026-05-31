ca643c20ebae3fe5b29076acb31f0f83f3f39ecd
theeyad <eyadosman35@gmail.com>
2026-06-01 00:03:49 +0300

    finished all ui elements and will start logic

--- todo-app/src/components/TodoAdd/TodoAdd.jsx
+++ todo-app/src/components/TodoAdd/TodoAdd.jsx
@@ -1,5 +1,26 @@
 import "./TodoAdd.css";
 
 export default function TodoAdd() {
-  return <></>;
+  return (
+    <>
+      <form
+        method="post"
+        className="add-todo-container"
+        onClick={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          name="todo-text"
+          id="todoText"
+          className="add-todo-input"
+          placeholder="إضافة مهمة جديدة..."
+        />
+        <button type="submit" className="add-todo-button">
+          إضافة
+        </button>
+      </form>
+    </>
+  );
 }
--- todo-app/src/components/TodosFilter/TodosFilter.jsx
+++ todo-app/src/components/TodosFilter/TodosFilter.jsx
@@ -1,5 +1,13 @@
 import "./TodosFilter.css";
 
 export default function TodosFilter() {
-  return <></>;
+  return (
+    <>
+      <div className="filter-container">
+        <div className="filter-button">الكل</div>
+        <div className="filter-button">مكتمل</div>
+        <div className="filter-button">غير مكتمل </div>
+      </div>
+    </>
+  );
 }
