01ce3e2a3e827965a7fb83363c976a602c0c53ba
theeyad <eyadosman35@gmail.com>
2026-06-05 17:05:33 +0300

    filter logic

--- todo-app/src/components/TodoBox/TodoBox.jsx
+++ todo-app/src/components/TodoBox/TodoBox.jsx
@@ -2,15 +2,23 @@ import Heading from "../Heading/Heading";
 import Todo from "../Todo/Todo";
 import TodoAdd from "../TodoAdd/TodoAdd";
 import TodosFilter from "../TodosFilter/TodosFilter";
-import "./TodoBox.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TodosContext } from "@/Contexts/TodosContext";
+import "./TodoBox.css";
 
 export default function TodoBox() {
   const { todos } = useContext(TodosContext);
+  const [activeFilter, setActiveFilter] = useState("all");
+
+  const customTodos =
+    activeFilter === "done"
+      ? todos.filter((t) => t.isCompleted)
+      : activeFilter === "undone"
+        ? todos.filter((t) => !t.isCompleted)
+        : todos;
 
   function renderTodos() {
-    const todosRendered = todos.map((todo) => {
+    const todosRendered = customTodos.map((todo) => {
       return <Todo key={todo.id} todo={todo} />;
     });
 
@@ -21,7 +29,10 @@ export default function TodoBox() {
     <>
       <div className="box rounded-xl px-3 py-5">
         <Heading />
-        <TodosFilter />
+        <TodosFilter
+          activeFilter={activeFilter}
+          setActiveFilter={setActiveFilter}
+        />
         {todos.length > 0 ? renderTodos() : null}
         <TodoAdd />
       </div>
--- todo-app/src/components/TodosFilter/TodosFilter.jsx
+++ todo-app/src/components/TodosFilter/TodosFilter.jsx
@@ -1,12 +1,39 @@
 import "./TodosFilter.css";
 
-export default function TodosFilter() {
+export default function TodosFilter({ activeFilter, setActiveFilter }) {
+  function renderAllTodos() {
+    setActiveFilter("all");
+  }
+
+  function renderCompletedTodos() {
+    setActiveFilter("done");
+  }
+
+  function renderUncompletedTodos() {
+    setActiveFilter("undone");
+  }
+
   return (
     <>
       <div className="filter-container">
-        <div className="filter-button">الكل</div>
-        <div className="filter-button">مكتمل</div>
-        <div className="filter-button">غير مكتمل </div>
+        <div
+          className={`filter-button ${activeFilter === "all" ? "active" : ""}`}
+          onClick={() => renderAllTodos()}
+        >
+          الكل
+        </div>
+        <div
+          className={`filter-button ${activeFilter === "done" ? "active" : ""}`}
+          onClick={() => renderCompletedTodos()}
+        >
+          مكتمل
+        </div>
+        <div
+          className={`filter-button ${activeFilter === "undone" ? "active" : ""}`}
+          onClick={() => renderUncompletedTodos()}
+        >
+          غير مكتمل{" "}
+        </div>
       </div>
     </>
   );
